6144b116087978c9534c82e4e2b5a91059acb7e2
Sergey Ruppel <sergeyruppel@users.noreply.github.com>
2024-03-12 15:03:37 +0700

    Develop (#1)
    * Images added
    
    * Update main.dart
    
    * Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,54 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: const Text('Ask Me Anything'),
+          backgroundColor: Colors.grey[800],
+          foregroundColor: Colors.white,
+          centerTitle: true,
+        ),
+        body: const BallPage(),
+      ),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return const Ball();
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: Center(
+        child: TextButton(
+          onPressed: () {
+            setState(() {
+              ballNumber = Random().nextInt(5) + 1;
+            });
+          },
+          child: Image.asset('images/ball$ballNumber.png'),
         ),
       ),
     );
